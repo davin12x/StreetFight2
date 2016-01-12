9ee6c84be2cb1ab7a531fb329985a9af5b47efce
Lalit Bagga <lbagga@icloud.com>
2016-01-12 11:45:02 -0500

    Initial Commit

---
+++ StreetFight2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreetFight2
+//
+//  Created by Lalit on 2016-01-12.
+//  Copyright © 2016 Bagga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
